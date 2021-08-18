55ac25a866176e2d940087a8682f9f52b9f402a9
tharvey <tharveyster@gmail.com>
2021-08-18 12:53:50 -0400

    Added route to get all tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,7 +29,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id! ' });
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
 

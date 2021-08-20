ae874906bf601d4d8847052a2b26256a0f691b2d
tharvey <tharveyster@gmail.com>
2021-08-20 15:38:10 -0400

    Added a route to create a new product

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'cascade',
+  onUpdate: 'cascade',
 })
 
 // Categories have many Products

31d47b73bd929b93af3b3321cfc14a1fea6574f6
tharvey <tharveyster@gmail.com>
2021-08-20 15:24:11 -0400

    Added a route to delete a product by id

--- models/index.js
+++ models/index.js
@@ -7,24 +7,27 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'cascade',
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  onDelete: 'cascade',
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
+  onDelete: 'cascade',
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
+  onDelete: 'cascade',
 })
 
 module.exports = {
